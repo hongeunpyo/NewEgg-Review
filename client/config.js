c83d618e23770336e7a71f0f0271240ee2d740e1
hongeunpyo <eunpyohong@gmail.com>
2019-03-16 10:23:55 -0400

    build for server

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     // awsReview: 'localhost:3009'
-    // awsReview: 'ec2-18-191-111-73.us-east-2.compute.amazonaws.com'
+    awsReview: 'ec2-18-191-111-73.us-east-2.compute.amazonaws.com'
     // awsReview: 'a680709ab474911e9848112113241686-873489365.us-east-1.elb.amazonaws.com'
-    awsReview: 'localhost'
+    // awsReview: 'localhost'
 }
